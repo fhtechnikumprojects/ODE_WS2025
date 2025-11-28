a6afb1e8e76143f78a5ce67ec96edf1bba0562b8
Thanathan Wongkantharaphai <e11811382@student.tuwien.ac.at>
2025-11-28 10:24:00 +0100

    changes of Launcher.java

--- src/main/java/org/example/project_wobimich/Launcher.java
+++ src/main/java/org/example/project_wobimich/Launcher.java
@@ -10,8 +10,8 @@ public class Launcher {
     public static void main(String[] args) throws Exception {
         //Application.launch(HelloApplication.class, args);
 
-        String street = "Kärntner Straße";
-        String number = "5";
+        String street = "Neuer Markt";
+        String number = "3";
         Location location = new Location(street, number);
 
         File filePath = new File("D:/Users/Kharim/ODE_WS25/src/main/resources/org/example/project_wobimich/data/ADRESSENOGD_1010_filtered.json");

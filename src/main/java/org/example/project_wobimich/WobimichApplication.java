e03d92955fe4d66ecbdb52fd647ba1ba0b5c4a74
ic24b118 <ic24b118@technikum-wien.at>
2026-01-14 20:49:25 +0100

    Logo in WobimichUI eingefügt, Size angepasst

--- src/main/java/org/example/project_wobimich/WobimichApplication.java
+++ src/main/java/org/example/project_wobimich/WobimichApplication.java
@@ -3,15 +3,23 @@ package org.example.project_wobimich;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.project_wobimich.model.LineStation;
+import org.example.project_wobimich.model.Station;
 import org.example.project_wobimich.ui.WobimichUI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WobimichApplication extends Application {
     @Override
     public void start(Stage stage) {
+
         WobimichUI wobimichUI = new WobimichUI();
-        Scene wobimichScene = new Scene(wobimichUI.createScene());
+
+        Scene wobimichScene = new Scene(wobimichUI.createScene(), 800, 600);
         stage.setScene(wobimichScene);
-        stage.setTitle("Wobimich?!");
+        stage.setTitle("Wobimich Test");
         stage.show();
     }
 
@@ -19,3 +27,4 @@ public class WobimichApplication extends Application {
         launch();
     }
 }
+
